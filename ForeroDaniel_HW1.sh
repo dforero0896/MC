bf7cc298e1f1ecc1234f6e4118f6ab1770ec7f82
Daniel Forero <df.forero10@gmail.com>
2015-05-29 15:27:13 -0500

    primera version punto uno tarea

--- ForeroDaniel_HW1.sh
+++ ForeroDaniel_HW1.sh
@@ -1,1 +1,10 @@
 #!/bit/bash
+figlet arXiv
+read kwrd
+
+echo =====================================
+echo Searching the arXiv for the new stuff.
+echo http://arxiv.org/list/astro-ph/new
+curl -s http://arxiv.org/list/astro-ph/new | grep -i  $kwrd | grep Title
+echo =====================================
+echo keyword: $kwrd
