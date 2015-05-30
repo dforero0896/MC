9470a620d1c94dd6f7bc92e42b2551824506fec5
Daniel Forero <df.forero10@gmail.com>
2015-05-30 18:07:37 -0500

    adición del ejecutable del primer punto

---
+++ Tareas/HW1/arxiv.sh
@@ -0,0 +1,11 @@
+#!/bit/bash
+figlet arXiv
+read kwrd
+
+echo =====================================
+echo Searching the arXiv for the new stuff.
+echo http://arxiv.org/list/astro-ph/new
+curl -s http://arxiv.org/list/astro-ph/new | grep -i  $kwrd | grep Title
+echo =====================================
+echo keyword: $kwrd
+echo =====================================
