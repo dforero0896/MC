a892cdacf3ccddc64af21809bd9b03b5f276aa39
Daniel Forero <df.forero10@gmail.com>
2015-05-30 19:39:06 -0500

    update

--- Tareas/HW1/arxiv.sh
+++ Tareas/HW1/arxiv.sh
@@ -5,7 +5,11 @@ read kwrd
 echo =====================================
 echo Searching the arXiv for the new stuff.
 echo http://arxiv.org/list/astro-ph/new
-curl -s http://arxiv.org/list/astro-ph/new | grep -i  $kwrd | grep Title
+
+curl -s http://arxiv.org/list/astro-ph/new | grep -i  $kwrd | grep Title > archivoConLosTitulos.txt
 echo =====================================
 echo keyword: $kwrd
 echo =====================================
+nlines= wc -l archivoConLosTitulos.txt  | sed 's/archivoConLosTitulos.txt/articles found./g'
+curl -s http://arxiv.org/list/astro-ph/new | grep -i  $kwrd | grep Title | sed 's/<span class="descriptor">Title:<\/span>/-/g'
+rm archivoConLosTitulos.txt
