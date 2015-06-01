c18b4aad684e76a6083daffcc954f9a60ccae172
Daniel Forero <df.forero10@gmail.com>
2015-06-01 11:18:09 -0500

    Update arxiv, bruno

--- Tareas/HW1/bruno.sh
+++ Tareas/HW1/bruno.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-echo Se calculará el número de planetas extrasolares en el archivo.
-awk '{print $1}'
+echo Se calculará el número de planetas extrasolares en el archivo. \\n
+ninit=$(awk -F","  '{ print $1 }' kepler.csv | wc -l)
+nfin=$((ninit-1))
+echo Hay $nfin planetas en el archivo.
+
+echo Se calculará ahora el número de planetas con una masa menor a una centésima de la masa de Júpiter
+
+#nmasa= $(awk -F ","  '{ if($2 < 0.01 && $2 != 0) print $2 }' kepler.csv | sed '/^$/d' | wc -l)
+
+echo Se devolverá el nombre del planeta con menor periodo orbital.
+sort --field-separator="," --key=6 -n kepler.csv | awk -F"," 'NR==2 {print $1}'
+echo es el planeta con menor periodo orbital. 
