88ce00ec2c3d37fba079b9925ac52607ffa0075d
Daniel Forero <df.forero10@gmail.com>
2015-06-01 19:19:35 -0500

    Tarea Finalizada

--- Tareas/HW1/stardate.sh
+++ Tareas/HW1/stardate.sh
@@ -11,8 +11,6 @@ echo $history
 echo ==============================================
 yr1=$((2015-$1))
 yr2=$((2015-$1+1))
-echo $yr1, $yr2
-
 echo LOOK AT THE FOLLOWING STARS:
 awk -F "," '{if(NR==1) print $2, $8, $9}' Hyg.csv| sed 's/hip/HIP_No./g'| sed 's/ra/RA\/°/g'| sed 's/dec/DEC\/°/g' > temporal.csv
 awk -v y1=$yr1 -v y2=$yr2 -F "," '{if($10>y1 && $10 <y2) print $2, $8, $9}' Hyg.csv >> temporal.csv
