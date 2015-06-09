ba5b42dc7d5b41804dcc7d3459f93bfca48d3e98
Daniel Forero <df.forero10@gmail.com>
2015-06-09 09:21:27 -0500

    finalización 3.a

--- Tareas/HW2/flip.sh
+++ Tareas/HW2/flip.sh
@@ -7,18 +7,7 @@ len=${#str}
 lenf=$(($len+1))
 echo $len
 echo $letter1
-echo $letter2 
-#for i in $(seq 0 $len); do
-    letter=${str:$i:1}
-        n=$(($i+1))
-	if [ $letter == $letter1 ]; then
-	  echo primer 
-#sed -e "s/./$letter2/$n" $file > a.txt
-       	break
-	elif [ $letter==$letter2 ]; then
-	    echo segundo
- #      sed -e "s/./$letter1/$n" $file > b.txt
-	break
-       fi
- echo 1
-done
+echo $letter2
+
+sed "s/$letter1/TEMPORARYONE/g;s/$letter2/TEMPORARYTWO/g;s/TEMPORARYONE/$letter2/g;s/TEMPORARYTWO/$letter1/g" $file
+
