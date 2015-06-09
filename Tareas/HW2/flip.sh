6c1270dc50fce03dd9dc04f727abed80b37c0eb3
Daniel Forero <df.forero10@gmail.com>
2015-06-09 08:59:00 -0500

    antes de rehacer punto 3

--- Tareas/HW2/flip.sh
+++ Tareas/HW2/flip.sh
@@ -2,5 +2,23 @@
 letter1=$1
 letter2=$2
 file=$3
-
-sed -E 's/$letter1/$letter2/g' $file > temporal.txt
+str=$(cat $file)
+len=${#str}
+lenf=$(($len+1))
+echo $len
+echo $letter1
+echo $letter2 
+#for i in $(seq 0 $len); do
+    letter=${str:$i:1}
+        n=$(($i+1))
+	if [ $letter == $letter1 ]; then
+	  echo primer 
+#sed -e "s/./$letter2/$n" $file > a.txt
+       	break
+	elif [ $letter==$letter2 ]; then
+	    echo segundo
+ #      sed -e "s/./$letter1/$n" $file > b.txt
+	break
+       fi
+ echo 1
+done
